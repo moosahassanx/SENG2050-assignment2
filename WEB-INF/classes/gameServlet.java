a74f7f7640ab189c1baee5bc2883083f3bc6b1cc
Moosa Hassan <moosa7021@gmail.com>
2020-04-22 16:26:42 +1000

    making new servlet for testing cell

---
+++ WEB-INF/classes/gameServlet.java
@@ -0,0 +1,27 @@
+// Import required java libraries
+import java.io.*;
+import java.util.Random;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+import beans.UserBean;
+import beans.GameBean;
+
+@WebServlet(urlPatterns = {"/gameServlet"})
+public class gameServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      HttpSession session = request.getSession();
+      String username = request.getParameter("username");
+      String difficulty = request.getParameter("gameDifficulty");
+      
+      GameBean gameBeanObject = new GameBean(difficulty);
+      session.setAttribute("minesweeper", gameBeanObject);
+      
+      response.sendRedirect("game.jsp");
+	}
+}
