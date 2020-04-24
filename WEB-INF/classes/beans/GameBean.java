c69d88c3ef152d607375f68a44423de374a239d3
Moosa Hassan <moosa7021@gmail.com>
2020-04-24 16:32:33 +1000

    working on game mechanics

--- WEB-INF/classes/beans/GameBean.java
+++ WEB-INF/classes/beans/GameBean.java
@@ -16,6 +16,9 @@ public class GameBean implements Serializable{
     private Cells[][] cellArray;
     private UserBean UserObject;
 
+    private int rowClicked;
+    private int colClicked;
+
 
     //constructors 
     public GameBean() { }
@@ -40,7 +43,9 @@ public class GameBean implements Serializable{
         }
     }
 
-    public void testCell(){
+    public void testCell(String c){
+        int rowA = Integer.parseInt(c);
+        
         /*
         if(cellSelected.isMine()){
             - pop-up box saying you fucked up
@@ -71,3 +76,88 @@ public class GameBean implements Serializable{
         return this.column;
     }
 }
+
+/* JOSH GAME MECHANICS
+    public void tableMaker(int x)
+    {
+        cells = new cellBeans[x][x];
+        do{
+            for (int i = 0; i < x; i++)
+            {
+                for(int j = 0; j < x; j++)
+                {
+                    double willItBomb = Math.random();
+                    if (willItBomb < 0.4)
+                    {
+                        cells[i][j].setHasBomb(true);
+                        bombTotal++;
+                    }
+                    else
+                    {
+                        cells[i][j].setHasBomb(false);
+                    }
+                    //Have a grid, manually allocate the first 100 as mines, then shuffle the table
+                }
+            }
+        } while(bombTotal <= ((x*x)/4));
+            //Creating the numbers that show how many bombs are in the vincinity of obsceninty in my eyes
+            
+            for(int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; i++)
+                {
+                    checkBombs(i, j);
+                }
+            }
+        }
+
+    public int checkBombs(int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= heightWidth || j >= heightWidth)
+        {
+            return 0;
+        }
+        int surBombTotal = 0;
+        for (int k = -1; k < 2; k++)
+        {
+            for (int l = -1; l < 2; l++)
+            {
+                if ((k+i) < 0 || (l+j) < 0 || (k+i) >= heightWidth || (l+j) >= heightWidth)
+                {
+                    continue;
+                }
+                else
+                {
+                    surBombTotal++;
+                }
+            }
+        }
+        cells[i][j].setSurBombs(surBombTotal);
+        return surBombTotal;
+    }
+
+    public void openDaSpaces(int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= heightWidth || j >= heightWidth)
+        {
+            return;
+        }
+        if (cells[i][j].getIsOpen())
+        {
+            return;
+        }
+        cells[i][j].setIsOpen(true);
+        if (checkBombs(i,j) != 0)
+        {
+            return;
+        }
+        openDaSpaces((i-1), (j-1));
+        openDaSpaces((i), (j-1));
+        openDaSpaces((i+1), (j-1));
+        openDaSpaces((i-1), (j));
+        openDaSpaces((i+1), (j));
+        openDaSpaces((i+1), (j+1));
+        openDaSpaces((i-1), (j+1));
+        openDaSpaces((i), (j+1));
+    }
+*/
